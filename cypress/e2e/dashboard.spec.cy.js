534363ed3a98cdfb9b7b5acabba4a1d607328475
Mathis Beau <mathis.beau@efrei.net>
2022-12-12 15:13:37 +0100

    fix cypress

--- cypress/e2e/dashboard.spec.cy.js
+++ cypress/e2e/dashboard.spec.cy.js
@@ -3,7 +3,10 @@ describe('Dashboard', () => {
     cy.visit('http://127.0.0.1:8501/')
     cy.wait(500)
     // Check that the title and the input exist
-    cy.get('[id="prediction-de-prix-de-maison"]').should('exist')
+    
+    cy.get('#prediction-de-prix-de-maison')
+    
+    //cy.get('[id="prediction-de-prix-de-maison"]').should('exist')
     cy.get('[data-baseweb="input"]').should('exist')
 
     // Check the value of the input and click on the plus button
