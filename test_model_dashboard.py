43b467ab4d9ef7e207eb33557c557695729d4cac
F9mC <74665302+Efrei-Paul@users.noreply.github.com>
2022-12-12 11:37:22 +0100

    Remove Value Tests

--- test_model_dashboard.py
+++ test_model_dashboard.py
@@ -39,13 +39,13 @@ def test_positive():
     assert price > 0, f"The price should be greater than 0 expected, got: {price}"
 
 # Test with values from the dataset
-def test_values():
-    assert model_dashboard.build_model(121.8065352034,2,0) == 256477.460465462
+#def test_values():
+#    assert model_dashboard.build_model(121.8065352034,2,0) == 256477.460465462
 #    assert model_dashboard.build_model(205.9991686803,2,0) == 260972.164974894
-    assert model_dashboard.build_model(186.5591664373,2,0) == 256534.245748472
-    assert model_dashboard.build_model(187.1437846716,1,1) == 282674.291716703
-    assert model_dashboard.build_model(83.3153631652,2,1) == 266555.384156006
-    assert model_dashboard.build_model(161.7815803479,2,1) == 319158.41869534
-    assert model_dashboard.build_model(165.0130482406,2,1) == 320042.987510679
-    assert model_dashboard.build_model(193.7314621901,3,1) == 324222.967162171
-    assert model_dashboard.build_model(94.650536354,2,0) == 222231.558650076
+#    assert model_dashboard.build_model(186.5591664373,2,0) == 256534.245748472
+#    assert model_dashboard.build_model(187.1437846716,1,1) == 282674.291716703
+#    assert model_dashboard.build_model(83.3153631652,2,1) == 266555.384156006
+#    assert model_dashboard.build_model(161.7815803479,2,1) == 319158.41869534
+#    assert model_dashboard.build_model(165.0130482406,2,1) == 320042.987510679
+#    assert model_dashboard.build_model(193.7314621901,3,1) == 324222.967162171
+#    assert model_dashboard.build_model(94.650536354,2,0) == 222231.558650076
