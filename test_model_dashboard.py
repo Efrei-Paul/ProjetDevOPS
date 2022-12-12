29badd0049974d986267fcd46734a3e14586e17c
Paul <paul.cabon@protonmail.com>
2022-12-12 13:33:11 +0100

    debug

--- test_model_dashboard.py
+++ test_model_dashboard.py
@@ -1,45 +1,51 @@
-import streamlit as st 
-import joblib
+#!/usr/bin/python3
 
-@st.cache
-def load_model():
-    return joblib.load('regression.joblib')
+import model_dashboard
+import pytest
 
-def build_model(taille, nb_rooms, garden):
-    if type(taille) != float and type(taille) != int:
-        raise Exception("Size must be a numeric value")
-    if taille < 0:
-        raise Exception("Size must be positive")
+# Test if the function exists
+def test_model_dashboard():
+    assert model_dashboard.build_model(0,0,0)
 
-    if type(nb_rooms) != int or (nb_rooms != int(nb_rooms)):
-        raise Exception("Nb_rooms must be an integer")
-    if nb_rooms < 0:
-        raise Exception("Nb_rooms must be positive")
+# Test if wrong type raises an exception
+def test_type_taille():
+    with pytest.raises(Exception):
+        model_dashboard.build_model([1],1,1)
 
-    if type(garden) != float and type(garden) != int:
-        raise Exception("Garden must be a numeric value")
-    if garden < 0:
-        raise Exception("Garden must be positif or null")
+def test_type_nb_rooms():
+    with pytest.raises(Exception):
+        model_dashboard.build_model(1,[1],1)
 
-    model = load_model()
+def test_type_garden():
+    with pytest.raises(Exception):
+        model_dashboard.build_model(1,1,[1])
 
-    if taille <= 0:
-        return 'mettre taille correcte'
-    if nb_rooms <= 0:
-        return "mettre nombre de chambre correct"
+# Test if wrong value raises an exception
+def test_taille_negative():
+    with pytest.raises(Exception):
+        model_dashboard.build_model(-1,1,1)
 
-    if taille > 0 and nb_rooms > 0:
-        X = [[taille, nb_rooms, garden]]
-        return model.predict(X)[0]
+def test_nb_rooms_negative():
+    with pytest.raises(Exception):
+        model_dashboard.build_model(1,-1,1)
 
+def test_garden_negative():
+    with pytest.raises(Exception):
+        model_dashboard.build_model(1,1,-1)
 
+# Test if price is positive
+def test_positive():
+    price = model_dashboard.build_model(230,2,1)
+    assert price > 0, f"The price should be greater than 0 expected, got: {price}"
 
-def main():
-	st.title("Prediction de prix de maison")
-	taille = st.number_input("Taille maison")
-	nb_rooms = st.number_input("Nombre de chambre")
-	garden = st.number_input("Y a un jardin")
-	st.write("le prix de la maison est : {}".format(build_model(taille, nb_rooms, garden)))	
-
-if __name__ == "__main__":
-    main()
+# Test with values from the dataset
+# def test_values():
+#    assert model_dashboard.build_model(121.8065352034,2,0) == 256477.460465462
+#    assert model_dashboard.build_model(205.9991686803,2,0) == 260972.164974894
+#    assert model_dashboard.build_model(186.5591664373,2,0) == 256534.245748472
+#    assert model_dashboard.build_model(187.1437846716,1,1) == 282674.291716703
+#    assert model_dashboard.build_model(83.3153631652,2,1) == 266555.384156006
+#    assert model_dashboard.build_model(161.7815803479,2,1) == 319158.41869534
+#    assert model_dashboard.build_model(165.0130482406,2,1) == 320042.987510679
+#    assert model_dashboard.build_model(193.7314621901,3,1) == 324222.967162171
+#    assert model_dashboard.build_model(94.650536354,2,0) == 222231.558650076
